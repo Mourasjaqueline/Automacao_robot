fde8c542b17c74ed85d7e14cfe815323395ee605
Jaqueline Moura <155014803+Mourasjaqueline@users.noreply.github.com>
2025-04-24 18:02:00 -0300

    Update run_tests.ps1

--- run_tests.ps1
+++ run_tests.ps1
@@ -1,7 +1,3 @@
-# run_tests.ps1
-robot --outputdir results --listener allure_robotframework testjaque.robot
-allure generate results -o allure-report --clean
-Start-Process "allure-report\index.html"
 
 
 
